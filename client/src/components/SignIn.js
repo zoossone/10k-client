25a2f64d572c636d9c0aef5a658e7d01bb26edb8
Kim SungJin <gotninja0102@gmail.com>
2021-06-11 00:33:37 +0900

    complete signin

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,13 +1,37 @@
 import React, { useState } from 'react';
 import axios from 'axios'
+import { Link } from 'react-router-dom';
 
-const SignIn = () => {
-    const [id, setId] = useState('')
+const SignIn = (props) => {
+    const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    const handleLogin = () => {
+        if (!email || !password) {
+            return alert('이메일과 비밀번호를 다 기입해주세요')
+        }
+        axios.post("https://10k/signin", {
+            email: email,
+            password: password
+        }).then((res) => {
+            props.loginHandler(res)
+        }).catch((err) => alert("이메일과 비밀번호를 다시 확인해주세요"))
+    }
 
     return (
         <div>
-            signin 페이지입니다.
+            <div>
+                <span>이메일</span>
+                <input type="email" onChange={(e) => setEmail(e.target.value)}></input>
+            </div>
+            <div>
+                <span>비밀번호</span>
+                <input type="password" onChange={(e) => setPassword(e.target.value)}></input>
+            </div>
+            <div>
+                <Link to="/signup">아이디가 없으신가요?</Link>
+            </div>
+            <button type="submit" onClick={handleLogin}>로그인</button>
         </div>
     );
 };
