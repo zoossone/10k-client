170b847f4b21f03e477a3ec0ffd7486111e6c96c
Kim SungJin <gotninja0102@gmail.com>
2021-06-11 11:31:33 +0900

    :memo: complete signup

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,14 +1,50 @@
+import axios from 'axios';
 import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
-const SignUp = () => {
+const SignUp = (props) => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [passwordCheck, setPasswordCheck] = useState('') // 이건 요청ㄴㄴ
+    const history = useHistory()
+
+    const handleClick = () => {
+        if(!name || !email || !password) {
+            return alert('모든 항목을 작성하셔야 합니다.')
+        } else if(password !== passwordCheck) {
+            return alert('비밀번호 재확인 부탁드립니다.')
+        }
+        axios.post("https://10k/signup", {
+            email: email,
+            name: name,
+            password: password
+        }).then(() => history.push('/signin'))
+        .catch(() => alert("이미 사용자가 있는 이메일 주소입니다."))
+    }
 
     return (
         <div>
-            signup 페이지입니다.
+            <span><b>회원가입</b></span>
+            <div>
+                <input type="text" placeholder="name" 
+                onChange={(e) => setName(e.target.value)}></input>
+            </div>
+            <div>
+                <input type="text" placeholder="email" 
+                onChange={(e) => setEmail(e.target.value)}></input>
+            </div>
+            <div>
+                <input type="password" placeholder="password" 
+                onChange={(e) => setPassword(e.target.value)}></input>
+            </div>
+            <div>
+                <input type="password" placeholder="password check"
+                onChange={(e) => setPasswordCheck(e.target.value)}></input>
+            </div>
+                <div><button onClick={handleClick}>회원가입하기!</button></div>
+                <hr/>
+                <div><Link to="/signin">혹쉬 아이디가 생각나셨습니까??</Link></div>
         </div>
     );
 };
